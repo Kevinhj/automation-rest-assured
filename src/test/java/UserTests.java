41ec41311775a8a7b4692cd27268b5d1fd54ce42
Kevin Hernandez <khkevinhernandez@gmail.com>
2021-02-28 12:08:35 -0600

    POJO for User tests

--- src/test/java/UserTests.java
+++ src/test/java/UserTests.java
@@ -1,3 +1,4 @@
+import model.User;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -10,11 +11,14 @@ public class UserTests {
 
     @Test
     public void Test_Create_User_Already_Exist(){
-        given()
+
+        User user = new User("Kevin", "kevintest@test.com","kevin" );
+
+        given().body(user)
                 .post(String.format("%s%s/register",baseUrl,resourcePath))
         .then()
-                .header("Content-Length", equalTo("50"))
+                .header("Content-Length", equalTo("57"))
                 .body("message", equalTo("User already exists"))
-                .statusCode(200);
+                .statusCode(406);
     } //Continue on min 45 Clase 3
 }
