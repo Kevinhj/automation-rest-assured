c59a43ead519c063a87d5d1350993721e173c98e
Kevin Hernandez <khkevinhernandez@gmail.com>
2021-03-09 22:12:44 -0600

    added crud for post and priority

--- src/test/java/PostTests.java
+++ src/test/java/PostTests.java
@@ -9,15 +9,15 @@ import specifications.RequestSpecs;
 import specifications.ResponseSpecs;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostTests extends BaseTest{
     private static String resourcePath = "/v1/post";
-    private static Integer createdPost = 0;
+    private static Integer postId = 0;
 
     @BeforeGroups("create_post")
     public void createArticle(){
 
-        Article testArticle = new Article(DataHelper.generateRandomTitle(), DataHelper.generateRandomContent());
         Post testPost = new Post(DataHelper.generateRandomTitle(), DataHelper.generateRandomContent());
 
         Response response = given()
@@ -26,7 +26,7 @@ public class PostTests extends BaseTest{
                 .post(resourcePath);
 
         JsonPath jsonPathEvaluator = response.jsonPath();
-        createdPost = jsonPathEvaluator.get("id");
+        postId = jsonPathEvaluator.get("id");
     }
 
     @Test
@@ -40,6 +40,56 @@ public class PostTests extends BaseTest{
                 .post(resourcePath)
                 .then()
                 .statusCode(200)
+                .body("message", equalTo("Post created"))
+                .spec(ResponseSpecs.defaultSpec());
+    }
+
+    @Test
+    public void Test_Get_All_Post_Success(){
+
+        given()
+                .spec(RequestSpecs.generateToken())
+                .get(resourcePath + "s")
+                .then()
+                .statusCode(200)
+                .spec(ResponseSpecs.defaultSpec());
+    }
+
+    @Test(groups = "create_post", priority = 1)
+    public void Test_Get_Post_By_Id_Success(){
+
+        given()
+                .spec(RequestSpecs.generateToken())
+                .get(resourcePath + "/" +  postId)
+                .then()
+                .statusCode(200)
+                .spec(ResponseSpecs.defaultSpec());
+    }
+
+    @Test(groups = "create_post", priority = 2)
+    public void Test_Update_Post_Success(){
+
+        Post testPost = new Post(DataHelper.generateRandomTitle(), DataHelper.generateRandomContent());
+
+        given()
+                .spec(RequestSpecs.generateToken())
+                .body(testPost)
+                .put(resourcePath + "/" +  postId)
+                .then()
+                .statusCode(200)
+                .body("message", equalTo("Post updated"))
+                .spec(ResponseSpecs.defaultSpec());
+    }
+
+    @Test(groups = "create_post", priority = 3)
+    public void Test_Delete_Post_Success(){
+
+        given()
+                .spec(RequestSpecs.generateToken())
+                .delete(resourcePath + "/" +  postId)
+                .then()
+                .statusCode(200)
+                .body("message",equalTo("Post deleted"))
                 .spec(ResponseSpecs.defaultSpec());
     }
 }
